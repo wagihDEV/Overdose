370d7d1db69fa03472ae948ac983b8d80fd2f210
wagih makary <wagihmacari@hotmail.com>
2024-04-09 10:10:51 +0300

    second comm

--- src/Menu.js
+++ src/Menu.js
@@ -11,11 +11,11 @@ const Menu = ({ items }) => {
                 const { id, title, img, desc, price } = item;
                 return (
                     <article key={id} className="menu-item">
-                        <img src={img} alt={title} className="photo" />
+                        
                         <div className="item-info">
                             <header>
                                 <h4>{title}</h4>
-                                <h4 className="price">{price}LBP</h4>
+                                <h4 className="price">{price}$</h4>
                             </header>
                             <p className="item-text">{desc}</p>
                         </div>
