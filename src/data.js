07a190caf368e6474f0b25ee7230ab14a2c0ffa3
wagih makary <wagihmacari@hotmail.com>
2024-04-09 14:35:19 +0300

    fifth

--- src/data.js
+++ src/data.js
@@ -149,12 +149,6 @@ const menu = [
         price: "55,000",
        
       },
-      {
-        id: 22,
-        title: 'Nutella',
-        category: 'Crepes',
-        price: "5",
-       
-      },
+      
   ];
   export default menu;
